4d1ec5a0ece8ddc6660232a4392ba07083a08580
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-01-31 19:26:34 +0530

    deploy and hardhatconfig

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,41 +1,20 @@
+const hre = require("hardhat");
+
 async function main() {
+  const NFT = await hre.ethers.getContractFactory("NFT");
+  const contract1 = await NFT.deploy(); //instance of contract
 
-  const [deployer] = await ethers.getSigners();
+  await contract1.deployed(); //now contract get deployed on hardhat blockchain
+  console.log("Address of NFT contract", contract1.address); //displaying address of contract
 
-  console.log("Deploying contracts with the account:", deployer.address);
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+  const Marketplace = await hre.ethers.getContractFactory("NFT");
+  const contract2 = await Marketplace.deploy(); //instance of contract
 
-  // deploy contracts here:
-  
-  
-  // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles();
+  await contract2.deployed(); //now contract get deployed on hardhat blockchain
+  console.log("Address of Marketplace contract", contract2.address); //displaying address of contract
 }
 
-function saveFrontendFiles(contract, name) {
-  const fs = require("fs");
-  const contractsDir = __dirname + "/../../frontend/contractsData";
-
-  if (!fs.existsSync(contractsDir)) {
-    fs.mkdirSync(contractsDir);
-  }
-
-  fs.writeFileSync(
-    contractsDir + `/${name}-address.json`,
-    JSON.stringify({ address: contract.address }, undefined, 2)
-  );
-
-  const contractArtifact = artifacts.readArtifactSync(name);
-
-  fs.writeFileSync(
-    contractsDir + `/${name}.json`,
-    JSON.stringify(contractArtifact, null, 2)
-  );
-}
-
-main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
